fd2ffd681fee4ae84464a82e029be4aa88b85e5e
EdvinHF <Hoed22rl@student.ju.se>
2025-02-16 23:36:50 +0100

    sad

--- other.js
+++ other.js
@@ -57,7 +57,16 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("scroll", updateBallPosition);
 
   */
-
+  window.addEventListener("pageshow", function (event) {
+    var historyTraversal =
+      event.persisted ||
+      (typeof window.performance != "undefined" &&
+        window.performance.navigation.type === 2);
+    if (historyTraversal) {
+      // Handle page restore.
+      window.location.reload();
+    }
+  });
   if (!navigation) {
     console.error("Navigation element not found!");
     return;
